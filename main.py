fc1ef3ca026469beb671d96d0f0038f8645b9055
murych <murychkun@yahoo.com>
2016-09-09 23:24:52 +0300

    fixed confirmation response

--- main.py
+++ main.py
@@ -6,19 +6,18 @@ CONFIRMATION_TOKEN = '55a22c56'
 
 app = Flask(__name__)
 
-slack = auth_slack()
 
-
-@app.route('/', methods=['POST'])
-def index(slack):
+@app.route('/callback', methods=['POST'])
+def callback():
 	if not request.json or 'type' not in request.json:
 		abort(400)
 	if request.json['type'] == 'confirmation':
 		return CONFIRMATION_TOKEN, 201
 	if request.json['type'] == 'wall_post_new':
+		slack = auth_slack()
 		object = request.json['object']
 		text = object['text']
 		slack.chat.postMessage('#admin', text, username='vk-bot')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=False)
