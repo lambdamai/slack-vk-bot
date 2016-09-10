673b1aa03dacb7c468dcf434b5274ecae7641f61
murych <murychkun@yahoo.com>
2016-09-10 20:19:40 +0300

    sending message with attachment as json #4

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
-from flask import Flask, request, abort
-from auth import auth_slack
+import json
+import os
 
-GROUP_DOMAIN = 'lambdafrela'
-CONFIRMATION_TOKEN = '55a22c56'
+from flask import Flask, request, abort
+from slacker import Slacker
+
+''' getting enviroment variables '''
 
 app = Flask(__name__)
-slack = auth_slack()
+
+
+def auth_slack():
+	bot_secret = os.environ.get('SLACK_BOT_SECRET')
+	# bot_secret = SLACK_BOT_SECRET
+	api = Slacker(bot_secret)
+
+	try:
+		api.auth.test()
+	except Exception as e:
+		print('not authed\n', e)
+
+	return api
+
+
+def create_msg(post):
+	text = post['text']
+	footer = 'Lambda ФРЭЛА | Лямбда'
+	ts = post['date']
+	return json.dumps([{
+		'fallback'   : '',
+		'color'      : '#0093DA',
+		'text'       : text,
+		'ts'         : ts,
+		'footer'     : footer,
+		'footer_icon': 'http://lambda-it.ru/static/img/lambda_logo_mid.png',
+	}]
+	)
+
+
+def send_message(channel, text='', attachments=None, as_user=True):
+	slack.chat.post_message(channel=channel,
+							text=text,
+							attachments=attachments,
+							as_user=as_user)
 
 
 @app.route('/callback/xE4sA', methods=['GET', 'POST'])
@@ -14,12 +50,18 @@ def callback():
 		abort(400)
 
 	if request.json['type'] == 'confirmation':
-		return CONFIRMATION_TOKEN
+		confirmation_token = os.environ.get('CONFIRMATION_TOKEN')
+		return confirmation_token
 
 	if request.json['type'] == 'wall_post_new':
-		text = request.json['object']['text']
-		slack.chat.post_message('#admin', text, as_user=True)
+		post = request.json['object']
+		channel = os.environ.get('CHANNEL')
+		text = os.environ.get('TEXT')
+		send_message(channel=channel, text=text, attachments=create_msg(post))
 		return 'ok', 200
+
+
+slack = auth_slack()
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", port=5000)
