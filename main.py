06c83401ddf530290a0b2717722e33d52f077616
murych <murychkun@yahoo.com>
2017-01-31 03:19:39 +0300

    табы на пробелы
    Signed-off-by: murych <murychkun@yahoo.com>

--- main.py
+++ main.py
@@ -6,29 +6,31 @@ from message import *
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/callback/xE4sA', methods=['GET', 'POST'])
 def callback():
-	if not request.json or 'type' not in request.json:
-		abort(400)
+    if not request.json or 'type' not in request.json:
+        abort(400)
 
-	if request.json['type'] == 'confirmation':
-		return confirmation_token_
+    if request.json['type'] == 'confirmation':
+        return confirmation_token_
 
-	if request.json['type'] == 'wall_post_new':
-		post = request.json['object']
+    if request.json['type'] == 'wall_post_new':
+        post = request.json['object']
 
-		slack_message = Slack(post=post).create_message()
-		Slack.send_message(auth=slack, channel=channel_, text=text_, attachments=slack_message)
+        attachments = Slack(post=post).create_attachments()
+        Slack.send_message(auth=slack, channel=channel_, text=text_,
+                           attachments=attachments)
 
-		return 'ok', 200
+        return 'ok', 200
 
 
 slack = auth_slack()
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
